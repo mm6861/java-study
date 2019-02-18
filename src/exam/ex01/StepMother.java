a2010cb73afa92d93296086dea3c95c7e705636e
mm6861 <woohoomi@nate.com>
2019-02-18 17:24:06 +0900

    2-18

---
+++ src/exam/ex01/StepMother.java
@@ -0,0 +1,8 @@
+package exam.ex01;
+
+public class StepMother {
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ src/exam/ex01/StringEqual.java
@@ -0,0 +1,15 @@
+package exam.ex01;
+
+public class StringEqual {
+	
+	public static void main(String[] args) {
+		String s1 = new String("a");
+		String s2 = new String("a");
+		String s3 = new String("a");
+		
+		s2 = s1;
+		System.out.println(s2==s1);
+	
+		
+	}
+}
